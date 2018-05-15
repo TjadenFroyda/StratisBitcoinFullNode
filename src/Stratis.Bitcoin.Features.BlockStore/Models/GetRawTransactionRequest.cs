c28e2ccac0c6e31792e40dc4acb8f69357ff6bb5
TjadenFroyda <skaal@protonmail.com>
2018-05-15 08:38:49 -0500

    Adding GetRawTransaction

---
+++ src/Stratis.Bitcoin.Features.BlockStore/Models/GetRawTransactionRequest.cs
@@ -0,0 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Stratis.Bitcoin.Features.BlockStore.Models
+{
+
+    public class GetRawTransactionRequest : RequestBase
+    {
+        [Required(AllowEmptyStrings = false)]
+        public string txid { get; set; }
+
+        public int verbose { get; set; }
+    }
+}
